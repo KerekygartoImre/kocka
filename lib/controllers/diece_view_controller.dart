7e063a0d9c92aeee8919758e9cf49d1554295cdb
keerekgyartomimre <imi20040508@gmail.com>
2023-10-09 10:08:35 +0200

    init commit

---
+++ lib/controllers/diece_view_controller.dart
@@ -0,0 +1,12 @@
+import 'dart:math';
+
+import 'package:get/get.dart';
+
+class DieceViewController extends GetxController {
+  int diceFaceNumber = 1;
+  void newRandomNumber() {
+    int random = Random().nextInt(6) + 1;
+    diceFaceNumber = random;
+    update();
+  }
+}
